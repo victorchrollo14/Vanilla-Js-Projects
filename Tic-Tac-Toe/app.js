297330f90a3d50bc47f45c234f172b493a40c548
Victor Immanuel <Victor20030214@gmail.com>
2023-02-19 21:18:53 +0530

    add all game logic using js

--- Tic-Tac-Toe/app.js
+++ Tic-Tac-Toe/app.js
@@ -7,11 +7,9 @@ const player = document.querySelector('.player');
 const gameOver = new Audio("Music/gameover.mp3");
 const ting = new Audio("Music/ting.mp3");
 const music = new Audio("Music/music.mp3");
-// music.play();
 
 // internal varibles
 const players = ["Player 1", "Player 2"];
-
 
 
 // changing focus on the selected symbol
@@ -29,7 +27,48 @@ symbols.forEach(symbol => {
 });
 
 
-// Displaying X and O.
+// reStart the game
+const reStart = () =>{
+    squares.forEach(square => {
+        if(!square.firstElementChild){
+            return;
+        }
+        square.firstElementChild.remove();
+        player.innerHTML = "Player 1";
+        i = 1;
+        // music.play();
+    })
+}
+
+
+// checking if any player has won
+const checkWin = () =>{
+    let winCombo = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+    ];
+    let winGame = false;
+
+    winCombo.forEach(e=>{
+        let box1 = squares[e[0]].innerText;
+        let box2 = squares[e[1]].innerText;
+        let box3 = squares[e[2]].innerText;
+        if (box1 === box2 && box2 === box3 && box3 === box1 && box1 != ''){
+            winGame = true;
+        }
+        
+    })
+    return winGame;
+}
+
+
+//Game Logic
 let i = 1;
 const X = document.createElement('div');
 X.classList.add('x-mark');
@@ -39,42 +78,45 @@ const O = document.createElement('div');
 O.classList.add('o-mark');
 O.innerHTML = "O";
 
+// music.play();
 squares.forEach(square => {
     square.addEventListener("click", (e) =>{
         if (i === 1 && !e.target.hasChildNodes()){
             let XClone = X.cloneNode(true);
             e.target.appendChild(XClone);
-            player.innerHTML = `${players[i]}`;
+            player.innerHTML = `${players[i]}'s Turn`;
         }
         else if(i === 0 && !e.target.hasChildNodes()){
             let OClone = O.cloneNode(true);
             e.target.appendChild(OClone);
-            player.innerHTML = `${players[i]}`;
+            player.innerHTML = `${players[i]}'s Turn`;
         }
-        
-    i = (i == 0) ? 1:0;
-       
+
+        // changing the player and symbols on each turn
+        i = (i == 0) ? 1:0; 
+        let isGameOver = checkWin();                               
+        if (isGameOver){
+            music.pause();
+            gameOver.play();
+            player.innerHTML = `${players[i]} wins the game`;
+            return;
+        }
+
+        ting.play();   
     })
 })
 
 
 // reset
-reset.addEventListener("click", function(e){
-    squares.forEach(square => {
-        if(!square.firstElementChild){
-            return;
-        }
-        square.firstElementChild.remove();
-
-    })
-})
+reset.addEventListener("click", reStart)
 
 
 
-// checking if any player has won
-const checkWin = () =>{
 
-}
+
+
+
+
 
 
 
